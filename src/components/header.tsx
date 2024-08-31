140f87934e8ea9d67bfa153cd2b7c0c457ee9fa2
proverbiallemon <lemon@Lemons-MacBook-Pro.local>
2024-08-31 13:01:23 -0400

    Changed div in nav to nav oops

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,16 +3,14 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header>
-       <div className="headermain">
       <h1>Artists Against Taupe</h1>
-      <nav>
+      <nav className="headermain">
         <a href="#what">Beyond Beige</a>
         <a href="#credo">Credo</a>
         <a href="#revolutionaries">Revolutionaries</a>
         <a href="#resources">Resources</a>
         <a href="#contact">Join the Movement</a>
       </nav>
-      </div>
     </header>
   );
 }
