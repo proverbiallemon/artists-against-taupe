055896877226b5c5a3eee197384c77071b0c84fe
proverbiallemon <lemon@Lemons-MacBook-Pro.local>
2024-08-31 14:41:04 -0400

    Added hamburger menu and css logic for it in both Header.tsx and index.css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
-      <h1>Artists Against Taupe</h1>
-      <nav className="headermain">
-        <a href="#what">Beyond Beige</a>
-        <a href="#credo">Credo</a>
-        <a href="#revolutionaries">Revolutionaries</a>
-        <a href="#partners">Partners</a>
-        <a href="#contact">Join the Movement</a>
-      </nav>
+        <h1>Artists Against Taupe</h1>
+        <div className={`nav-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="hamburger"></div>
+        <nav className={menuOpen ? 'open' : ''}>
+          <a href="#what">What</a>
+          <a href="#credo">Credo</a>
+          <a href="#revolutionaries">Revolutionaries</a>
+          <a href="#resources">Resources</a>
+          <a href="#contact">Join the Movement</a>
+        </nav>
+      </div>
     </header>
   );
 }
