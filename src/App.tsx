5cedbfbe089426ef138b04544e3bcebdf0162afe
proverbiallemon <lemon@Lemons-MacBook-Pro.local>
2024-08-31 10:25:18 -0400

    Commented out contact for for now as it's causing a build error

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import WhatWeStandFor from './components/WhatWeStandFor';
 import OurColorfulCredo from './components/OurColorfulCredo';
 import Revolutionaries from './components/Revolutionaries';
 import Resources from './components/Resources';
-import ContactForm from './components/ContactForm';
+// import ContactForm from './components/ContactForm';
 import './App.css'; // Ensure the global CSS is applied
 
 const App: React.FC = () => {
