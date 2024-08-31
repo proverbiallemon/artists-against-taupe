4e479a9c1050b48d007996c06245c8c164d0b74b
proverbiallemon <lemon@Lemons-MacBook-Pro.local>
2024-08-31 12:42:42 -0400

    Updated the verbage Tiff had changed

--- src/components/OurColorfulCredo.tsx
+++ src/components/OurColorfulCredo.tsx
@@ -5,9 +5,8 @@ const OurColorfulCredo: React.FC = () => {
     <section id="credo">
        <div className="container">
       <h2>Credo</h2>
-      <p>Life is hard.  The only thing we all have in common is that each of us used to be a baby, and babies love color, sound, and movement. It’s impossible to feel joy when your most basic needs aren’t met, so the opportunity that Safe Place offers for youth, the certainty that their needs are met and they are safe, also gives the opportunity for a bit of mental rest and decompression.  I’ve never found peace in an institutional looking environment, so I wanted to change things.  </p>
-      <p>Instead of a place where basic needs are met, I was inspired to go above and beyond with art and community and color, to also curate a space where the environment allows your brain a moment for peace, escape, and inspiration as well.</p>
-      <p>I envisioned the kind of space that doesn’t remind you that you need help.  I wanted to change the look of help. </p>
+      <p>Life is hard.  The only thing we all have in common is that each of us used to be a baby, and babies love color, sound, and movement. It’s impossible to feel joy when your most basic needs aren’t met, so the opportunity that Safe Place offers for youth, the certainty that their needs are met and they are safe, also gives the opportunity for a bit of mental rest and decompression.  I’ve never found peace in an institutional looking environment, so I wanted to change things.   </p>
+      <p>Witnessing the care of Safe Place Services, I was inspired to contribute with art, community, and color.I envisioned the kind of space that doesn’t remind you that you need help.  I wanted to change the look of help.  I wanted to create a space that also allows your brain to rest, escape, decompress, and inspire.</p>
 
     </div>
     </section>
