01a19514467d7361b06ce5633c39996628ba52a0
proverbiallemon <lemon@Lemons-MacBook-Pro.local>
2024-09-01 18:56:37 -0400

    Added new images in the carousel

--- src/components/WhatWeStandFor.tsx
+++ src/components/WhatWeStandFor.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Slideshow from './Slideshow'; // Ensure this path is correct based on your file structure
 
 const images = [
-  'https://picsum.photos/1200/400?random=2',
-  'https://picsum.photos/1200/400?random=3',
-  'https://picsum.photos/1200/400?random=4',
+  '/images/carousel1/Car1.1.webp',
+  '/images/carousel1/Car1.2.webp',
+  '/images/carousel1/Car1.3.webp',
+  '/images/carousel1/Car1.4.webp',
+  '/images/carousel1/Car1.5.webp',
+  '/images/carousel1/Car1.6.webp',
 ];
 
 const WhatWeStandFor: React.FC = () => {
@@ -16,7 +19,7 @@ const WhatWeStandFor: React.FC = () => {
         I am an artist, educator, and curator, in Louisville, KY. In 2011, my life took a transformative turn when I crossed paths at <a href="http://www.flamerun.com">Flame Run Glass Studio</a> with Nick Brown, a dedicated youth worker at the YMCA Safe Place, which caters to homeless and at-risk youth. Every Tuesday night since, I have been teaching art classes at Safe Place to the shelter house youth, unaware of the impact it would have on my own life.
       </p>
       <p>
-        Safe Place is unlike any other shelter available for at-risk youth. The youth will be the first ones to tell you that it feels different, and I hear often that most of the youth like it the best of any place they have ever been. The more I thought about it, the more I realized the youth deserve better. I have never been in a position where taupe or beige walls have made me feel better. I also realized that most of our clients are already institutionalized, spending most of their time moving from facility to facility, and it didn’t help them. Safe Place is dedicated to safety, shelter, and support. The number one goal is family unification, and strengthening the family as a whole when possible.
+        Safe Place is unlike any other shelter available for at-risk youth. The youth will be the first ones to tell you that it feels different, and I hear often that most of the youth like it the best of any place they have ever been. The more I thought about it, the more I realized the youth deserve better. I have never been in a position where taupe or beige walls have made me feel better. I also realized that most of our clients are already institutionalized, spending most of their time moving from facility to facility, and it didn't help them. Safe Place is dedicated to safety, shelter, and support. The number one goal is family unification, and strengthening the family as a whole when possible.
       </p>
       <p>
         My vision was to change the look of help. And so, my <a href="https://www.instagram.com/explore/tags/safeplacewalls/">#safeplacewalls</a>, <a href="https://www.instagram.com/explore/tags/artistsagainsttaupe/">#artistsagainsttaupe</a> journey began.
@@ -27,7 +30,6 @@ const WhatWeStandFor: React.FC = () => {
       <p>
         I also created and installed my most complicated artwork thus far, the "Mumplequarium". The old empty 50-gallon fish tank in the Shelter House has now been transformed into a moving LED foam and plastic zero-maintenance therapy tank. It is filled with Mumples, my imaginary friends, glowing butterflies, snails, and with fans to blow things around and animated LED lights that flicker and sparkle.
       </p>
-
       {/* Slideshow Component */}
       <Slideshow images={images} />
       </div>
